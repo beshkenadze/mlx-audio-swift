4615581fbbc4559b6c36f7165d3b966c0f7b850c
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-02-09 17:12:52 +0200

    fix: add missing MLXFast dependency to MLXAudioSTS target
    Closes beshkenadze/mlx-audio-swift#2

--- Package.swift
+++ Package.swift
@@ -125,6 +125,7 @@ let package = Package(
                 "MLXAudioSTT",
                 .product(name: "MLX", package: "mlx-swift"),
                 .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "MLXFast", package: "mlx-swift"),
                 .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
                 .product(name: "HuggingFace", package: "swift-huggingface"),
             ],
